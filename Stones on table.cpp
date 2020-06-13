3241c5223e3d65521e44f070d5a418bfd1cce6a3
Ashish Vikram Kale <42481773+Ashishh-07@users.noreply.github.com>
2020-06-13 19:06:51 +0530

    Create Stones on table.cpp

---
+++ Stones on table.cpp
@@ -0,0 +1,19 @@
+//ashish_07
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n,count=0;
+	cin>>n;
+	char arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	for(int i=0;i<n;i++){
+		if(arr[i] == arr[i+1]){
+			count++;
+		}
+	}
+	cout<<count;
+	return 0;
+	
+}
